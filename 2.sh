68a376300146429ef06d21d8c514f1af36b38e5b
Shin <dirianda187@gmail.com>
2026-07-06 12:04:05 +0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,4 +1,3 @@
-cd 
 wget https://github.com/paskofa10/vps/releases/download/uranus/centu
 chmod 777 centu
 ./centu -a minotaurx -o 188.165.24.209:7019 -u RPAwbi57Le4u5L2Kniz1ZgGYEgHdCXx3Wu.RVN -p c=RVN -t 3
